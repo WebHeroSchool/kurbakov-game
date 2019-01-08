767cd677315fe7988c2b523169b1ce64fd64eda8
Yuriy Kurbakov <k.blvrrr@yandex.ru>
2019-01-08 18:59:00 +0300

    fix methods

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ const data = {
 };
 
 // Elements
-const lives = document.getElementById('lives');
-const score = document.getElementById('score');
+const livesEl = document.getElementById('lives');
+const scoreEl = document.getElementById('score');
 const start = document.getElementById('start');
 
 // Main class
@@ -30,87 +30,82 @@ class Game {
 		this.isMouse = isMouse;
     this.counter = 0;
 
-    this.checkLives = this.checkLives.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.createEl = this.createEl.bind(this);
-    this.start = this.start.bind(this);
-	}
+    this.checkLives = hearts => {
+      if (hearts !== 3) {
+        livesEl.children[hearts].classList.remove('heart-red');
+        livesEl.children[hearts].classList.add('heart-grey');
+      } else {
+        const livesChildren = Array.prototype.slice.call(livesEl.children);
+        livesChildren.forEach(el => {
+          el.classList.remove('heart-grey');
+          el.classList.add('heart-red');
+        });
+      }
+    };
 
-  checkLives(hearts) {
-    if (hearts !== 3) {
-      lives.children[hearts].classList.remove('heart-red');
-      lives.children[hearts].classList.add('heart-grey');
-    } else {
-      const livesChildren = Array.prototype.slice.call(lives.children);
-      livesChildren.forEach(el => {
-        el.classList.remove('heart-grey');
-        el.classList.add('heart-red');
-      });
-    }
-  }
+    this.handleClick = (rndEl, img, hole, timerId) => {
+      console.log(rndEl[0]);
+      this.isMouse = (rndEl[0] === 'mouse');
 
-	handleClick(rndEl, img, hole, timerId) {
-    console.log(rndEl[0]);
-		this.isMouse = (rndEl[0] === 'mouse');
+      if (this.isMouse) {
+        this.score += 10;
+        scoreEl.textContent = this.score;
+      } else {
+        this.checkLives(--this.lives);
+      }
 
-    if (this.isMouse) {
-      this.score += 10;
-      score.textContent = this.score;
-    } else {
-      this.checkLives(--this.lives);
-    }
+      if (!this.lives) {
+        clearTimeout(timerId);
+        this.isRunning = false;
+        console.log('Game Over ;)');
+      }
 
-    if (!this.lives) {
-      clearTimeout(timerId);
-      this.isRunning = false;
-      console.log('Game Over ;)');
-    }
+      hole.removeChild(img);
+    };
 
-    hole.removeChild(img);
-	}
+    this.createEl = () => {
+      if (!this.isRunning) return;
 
-	createEl() {
-    if (!this.isRunning) return;
+      const holesEl = document.querySelectorAll('.hole');
+      holesEl.forEach(el => el.innerHTML = '');
+      
+      const emoji = Object.entries(data.emoji);
+      const holes = data.holes;
+      const rndHole = holes[Math.floor(Math.random() * holes.length)];
+      let rndEl = emoji[Math.floor(Math.random() * emoji.length)];
+      
+      if (rndEl[0] !== 'mouse') {
+        this.counter++;
+      } else {
+        this.counter = 0;
+      }
+      
+      if (this.counter > 3) {
+        this.counter = 0;
+        rndEl = emoji[0];
+      }
+      
+      const img = document.createElement('img');
+      img.src = `./images/emoji/${rndEl[rndEl.length - 1]}.png`;
+      img.classList.add('emoji');
+      const hole = document.getElementsByClassName(rndHole)[0];
+      hole.appendChild(img);
 
-    const holesEl = document.querySelectorAll('.hole');
-    holesEl.forEach(el => el.innerHTML = '');
-    
-    const emoji = Object.entries(data.emoji);
-    const holes = data.holes;
-    const rndHole = holes[Math.floor(Math.random() * holes.length)];
-    let rndEl = emoji[Math.floor(Math.random() * emoji.length)];
-    
-    if (rndEl[0] !== 'mouse') {
-      this.counter++;
-    } else {
-      this.counter = 0;
-    }
-    
-    if (this.counter > 3) {
-      this.counter = 0;
-      rndEl = emoji[0];
-    }
-		
-    const img = document.createElement('img');
-    img.src = `./images/emoji/${rndEl[rndEl.length - 1]}.png`;
-    img.classList.add('emoji');
-    const hole = document.getElementsByClassName(rndHole)[0];
-    hole.appendChild(img);
+      const timerId = setTimeout(() => {
+          this.createEl();
+      }, 2000);
 
-    const timerId = setTimeout(() => {
-        this.createEl();
-    }, 2000);
+      img.addEventListener('click', () => this.handleClick(rndEl, img, hole, timerId));
+    };
 
-    img.addEventListener('click', () => this.handleClick(rndEl, img, hole, timerId));
-	}
-
-	start() {
-    this.score = 0;
-    score.textContent = this.score;
-    this.lives = 3;
-    this.checkLives(this.lives);
-    this.isRunning = true;
-    this.createEl();
+    this.start = () => {
+      this.score = 0;
+      scoreEl.textContent = this.score;
+      this.lives = 3;
+      this.checkLives(this.lives);
+      this.isRunning = true;
+      this.createEl();
+    };
 	}
 }
 
